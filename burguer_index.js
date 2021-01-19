9340d2a08fb7b59a48ae2b9f698792f07769494e
ElsoloxD <76150949+ElsoloxD@users.noreply.github.com>
2021-01-18 21:29:30 -0300

    Add files via upload

---
+++ burguer_index.js
@@ -0,0 +1,53 @@
+
+let title = document.getElementById('titulo2')
+
+title.style.color = 'rgb(136, 255, 0)'
+
+
+var object1 = document.getElementById('sect4')
+
+
+
+var curso = prompt('¿Quiere que se le muestren opciones de cursos gratuitos?')
+
+
+
+curso.toLowerCase('si','no',)
+
+
+
+if (curso == 'no'){
+    object1.style.display = 'none'
+}
+
+
+
+var lenguajesProgramacion = ['PHP','HTML','CSS','JavaScript','Java','MongoDB','NodeJS'] 
+
+
+
+lenguajesProgramacion.toLowecase(['php','html','css','javascript','java','mongodb','nodejs'])
+
+var h2 = document.createElement('h2')
+
+contenido = document.createTextNode(lenguajesProgramacion)
+
+h2.appendChild(contenido)
+
+document.getElementById('pruebaa__').appendChild(h2)
+
+
+/*const d = document
+
+const jslist = d.getElementById('langList')
+
+const titleForjs = d.getElementById('titlejs')
+
+
+const itemlist = d.createElement('li')
+itemlist.textContent = 'lunes'
+
+jslist.appendChild(itemlist)
+
+console.log(itemlist)*/
+
